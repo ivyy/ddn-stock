444bda5dbd93078e4158303146707a90afbdda55
ChenZi <Ziwen.Chen@moodys.com>
2016-06-07 16:00:58 +0800

    adjust

--- src/main/java/com/ddn/stock/strategy/impl/SimpleStrategy.java
+++ src/main/java/com/ddn/stock/strategy/impl/SimpleStrategy.java
@@ -25,6 +25,8 @@ public class SimpleStrategy implements Strategy {
 
   private long lot = 0;
 
+  private double buyPrice = 0;
+
   public SimpleStrategy(String stockCode, Exchange[] exchanges) {
     this.stockCode = stockCode;
     Arrays.sort(exchanges, (s,t) -> s.getDate().compareTo(t.getDate()));
@@ -50,13 +52,14 @@ public class SimpleStrategy implements Strategy {
       String date = exchanges[i].getDate();
       double ma10 = ma10TimeSeries.valueAt(date);
       double lastMa10 = ma10TimeSeries.getPoints()[ma10TimeSeries.indexOfDate(date) - 1].getValue();
-      double macd = macdTimeSeries.getMacd().valueAt(date);
+     // double macd = macdTimeSeries.getMacd().valueAt(date);
+      double diff = macdTimeSeries.getDiff().valueAt(date);
 
-      if (close >= open && close > ma10 && macd > 0 && ma10 > lastMa10) {
+      if (close >= open && close > ma10  && ma10 > lastMa10 && (close / ma10 < 1.1) && diff > 0) {
         buy(date, close);
       }
 
-      if (ma10 < lastMa10 && close <= open && close < ma10 && (ma10 - close) / ma10 > 0.05) {
+      if ((ma10 < lastMa10 && close <= open && close < ma10) && ma10 / close > 1.1 || (buyPrice - close)/buyPrice >= 0.1 ) {
         if (lot > 0) {
           sell(date, close);
         }
@@ -76,14 +79,18 @@ public class SimpleStrategy implements Strategy {
     if (toBuy > 0) {
       this.principal = this.principal - toBuy * price * 100;
       this.lot += toBuy;
-      System.out.println("[" + date + "] buy " + toBuy + " lot at price: " + price + ", remaining principal: " + this.principal + " total lot: " + this.lot);
+      this.buyPrice = price;
+      System.out.println(String.format("[%s] buy %d lot at price: %.2f, remaining: %.2f, total lot: %d", date, toBuy, price, this.principal, this.lot));
+      //System.out.println("[" + date + "] buy " + toBuy + " lot at price: " + price + ", remaining principal: " + this.principal + " total lot: " + this.lot);
     }
   }
 
   private void sell(String date, double price) {
     this.principal = this.principal + price * this.lot * 100;
-    System.out.println("[" + date + "] sell " + lot + " lot at price: " + price + ", remaining principal: "
-        + this.principal + " earning " + (this.principal - this.oldPrincipal)/ this.oldPrincipal * 100 + "%");
+//    System.out.println("[" + date + "] sell " + lot + " lot at price: " + price + ", remaining principal: "
+//        + this.principal + " earning " + (this.principal - this.oldPrincipal)/ this.oldPrincipal * 100 + "%");
+    System.out.println(String.format("[%s] sell %d lot at price: %.2f, remaining: %.2f, earning %.2f%%", date, lot, price, principal,
+        (this.principal - this.oldPrincipal)/ this.oldPrincipal * 100));
     this.lot = 0;
     this.oldPrincipal = this.principal;
   }
